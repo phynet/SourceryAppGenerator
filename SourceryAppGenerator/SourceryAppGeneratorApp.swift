46744a4681a2be77023e6d8d89c929de5033800e
Sofia Swidarowicz <sofia.swidarowicz@gmail.com>
2024-09-19 12:13:31 +0200

    Initial Commit

---
+++ SourceryAppGenerator/SourceryAppGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceryAppGeneratorApp.swift
+//  SourceryAppGenerator
+//
+//  Created by Sofia Swidarowicz on 19/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceryAppGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
